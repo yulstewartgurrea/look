13af78d5c257e25ebdef53e7d190887ce4e205c9
yul gurrea <yul gurrea>
2016-12-18 02:03:02 +0800

    added products html

--- app/static/partials/admin/routes.js
+++ app/static/partials/admin/routes.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
+
 	$("#users").click(function(){
 		$("#addusers").load("addusers.html");
 	});
 
-	$('#categories').click(function(){
-		$("#addcategories").load("addcategories.html")
+	$('#products').click(function(){
+		$("#addproducts").load("addproducts.html");
 	});
+
+	$("#categories").click(function(){
+		$("#addcategories").load("addcategories.html");
+	})
 
 
 });
