02b0a7ceb5b2a075ca9a09b2416ab953cb74d324
yul gurrea <yul gurrea>
2016-12-19 16:15:17 +0800

    change page using jquery

--- app/app.py
+++ app/app.py
@@ -12,33 +12,6 @@ GENERIC_DOMAINS = "aero", "asia", "biz", "cat", "com", "coop", \
                   "name", "net", "org", "pro", "tel", "travel"
 
 
-def invalid(emailaddress, domains=GENERIC_DOMAINS):
-    """Checks for a syntactically invalid email address."""
-
-    # Email address must be 7 characters in total.
-    if len(emailaddress) < 7:
-        return True  # Address too short.
-
-    # Split up email address into parts.
-    try:
-        localpart, domainname = emailaddress.rsplit('@', 1)
-        host, toplevel = domainname.rsplit('.', 1)
-    except ValueError:
-        return True  # Address does not have enough parts.
-
-    # Check for Country code or Generic Domain.
-    if len(toplevel) != 2 and toplevel not in domains:
-        return True  # Not a domain name.
-
-    for i in '-_.%+.':
-        localpart = localpart.replace(i, "")
-    for i in '-_.':
-        host = host.replace(i, "")
-
-    if localpart.isalnum() and host.isalnum():
-        return False  # Email address is fine.
-    else:
-        return True  # Email address has funny characters.
 
 
 @app.route('/')
@@ -103,10 +76,6 @@ def get_users():
 def new_admin():
     jsn = json.loads(request.data)
 
-    if invalid(jsn['email_address']):
-        return jsonify({'status': 'error', 'message': 'Error'})
-
-
     res = spcall("new_admin", (
         jsn['email_address'],
         jsn['password']), True)
@@ -135,9 +104,6 @@ def get_admins():
 def new_establishment_personnel():
     jsn = json.loads(request.data)
 
-    if invalid(jsn['email_address']):
-        return jsonify({'status': 'error', 'message': 'Error'})
-
     res = spcall("new_establishment_personnel", (
         jsn['email_address'],
         jsn['password']), True)
@@ -163,9 +129,6 @@ def get_establishment_personnels():
 @app.route("/api/add/customer", methods=['POST'])
 def new_customer():
     jsn = json.loads(request.data)
-
-    # if invalid(jsn['email_address']):
-    # 	return jsonify({'status': 'error', 'message': 'Error'})
 
     res = spcall("new_customer", (
         jsn['email_address'],
